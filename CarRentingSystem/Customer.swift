2e255509603b0ff4a37ffeba148df8326d06210b
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-02-12 18:31:35 -0500

    [Nikita] working on Customer

--- CarRentingSystem/Customer.swift
+++ CarRentingSystem/Customer.swift
@@ -5,7 +5,6 @@
 //  Created by MacStudent on 2020-02-12.
 //  Copyright © 2020 MacStudent. All rights reserved.
 //
-
 import Foundation
 
 class Customer : Person{
@@ -16,5 +15,15 @@ class Customer : Person{
         super.init(id: id, firstName: firstName, lastName: lastName, gender: gender, birthDate: birthDate, age: age, mobileNumber: mobileNumber, email: email, userName: userName, password: password)
         self.address = address
         self.city = city
+    }
+    
+    override func Display() {
+        print("Customer ID \(id)")
+        print("Customer First Name \(firstName)")
+        print("Customer Last Name \(lastName)")
+        print("Customer ID \(id)")
+        print("Customer ID \(id)")
+        print("Customer ID \(id)")
+        print("Customer ID \(id)")
     }
 }
