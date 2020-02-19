95701a4515a06eb3fd7e6478937b547a619a9ed5
kamalpreet <kamal777682>
2020-02-19 17:27:14 -0500

    [kamal]

--- CarRentingSystem/Bus.swift
+++ CarRentingSystem/Bus.swift
@@ -8,17 +8,59 @@
 
 import Foundation
 
-class Bus: Vehicle
-{
-     var busType: String
-     var iswifiAvailable: Bool
-     var isAccessibiltyServiceAvailable: Bool
-    init(identificationNumber: String,description: String,isSelfDrive: Bool, manufacturerName: String,isInsured: Bool,vehicleNoOfSeats: Int,   insuranceProviderName: String, seat: Int,fuelType: String,ratePerDay: Double,
-    ratePerKm: Double, busType: String, iswifiAvailable:Bool,isAccessibiltyServiceAvailable: Bool){
-        self.busType = busType
-        self.iswifiAvailable = iswifiAvailable
-        self.isAccessibiltyServiceAvailable = isAccessibiltyServiceAvailable
-        super.init(identificationNumber: identificationNumber, description: description, isSelfDrive: isSelfDrive, manufacturerName: manufacturerName, isInsured: isInsured, vehicleNoOfSeats: vehicleNoOfSeats, insuranceProviderName: insuranceProviderName, seat: seat, fuelType: fuelType, ratePerDay: ratePerDay, ratePerKm: ratePerKm)
+class Bus : Vehicle
+     {
+          
+         var identificationNumber: String
+         var description: String
+         var isSelfDrive: Bool
+         var manufacturerName: String
+         var isInsured: Bool
+         var vehicleNoOfSeats: Int
+         var insuranceProviderName: String
+         var seat: Int
+         var fuelType: String
+         var ratePerDay: Double
+         var ratePerKm: Double
+         var busType: String
+         var iswifiAvailable: Bool
+         var isAccessibiltyServiceAvailable: Bool
+           
+         init(identificationNumber: String, description: String, isSelfDrive: Bool, manufacturerName: String, isInsured: Bool, vehicleNoOfSeats: Int, insuranceProviderName: String, seat: Int, fuelType: String, ratePerDay: Double, ratePerKm: Double, busType : String , iswifiAvailable : Bool, isAccessibiltyServiceAvailable: Bool)
+         {
+             self.identificationNumber = identificationNumber
+             self.description = description
+             self.isSelfDrive = isSelfDrive
+             self.manufacturerName = manufacturerName
+             self.isInsured = isInsured
+             self.vehicleNoOfSeats = vehicleNoOfSeats
+             self.insuranceProviderName = insuranceProviderName
+             self.seat = seat
+             self.fuelType = fuelType
+             self.ratePerDay = ratePerDay
+             self.ratePerKm = ratePerKm
+             self.busType = busType
+             self.iswifiAvailable = iswifiAvailable
+             self.isAccessibiltyServiceAvailable = isAccessibiltyServiceAvailable
+             
+         }
+          
+         func Display()
+         {
+             print("IdentificationNumber:        \(identificationNumber)")
+             print("Description:                 \(description)")
+             print("isSelfDrive:                 \(isSelfDrive)")
+             print("ManufacturerName:            \(manufacturerName)")
+             print("isInsured                       \(isInsured)")
+             print("VehicleNoOfSeats:                \(vehicleNoOfSeats)")
+             print("InsuranceProviderName            \(insuranceProviderName)")
+             print("Seat                             \(seat)")
+             print("FuelType:                        \(fuelType)")
+             print("RatePerDay:                      \(ratePerDay)")
+             print("ratePerKm:                       \(ratePerKm)")
+            }
+         
+     }
         
     }
     
--- CarRentingSystem/Vehicle.swift
+++ CarRentingSystem/Vehicle.swift
@@ -25,10 +25,10 @@ protocol Vehicle: IDisplay
     var ratePerKm: Double { get set}
     //var driver;
     
-    init(identificationNumber:String,description:String,isSelfDrive:Bool,
+   /* init(identificationNumber:String,description:String,isSelfDrive:Bool,
      manufacturerName: String,isInsured: Bool,vehicleNoOfSeats:Int,insuranceProviderName: String, seat: Int,fuelType: String,ratePerDay: Double,ratePerKm: Double)
 
-        /*self.identificationNumber = identificationNumber
+        self.identificationNumber = identificationNumber
         self.description = description
         self.isSelfDrive = isSelfDrive
         self.manufacturerName = manufacturerName
