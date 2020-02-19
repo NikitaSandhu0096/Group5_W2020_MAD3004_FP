6a93b07bd1b9ec088ec30718126b80e22e00815e
jyothithomas <jyothithomas.05@gmail.com>
2020-02-19 17:56:06 -0500

    [jyothi] commit car class

--- CarRentingSystem/Car.swift
+++ CarRentingSystem/Car.swift
@@ -1,51 +1,41 @@
-//
-//  Car.swift
-//  CarRentingSystem
-//
-//  Created by MacStudent on 2020-02-19.
-//  Copyright © 2020 MacStudent. All rights reserved.
-//
-
-import Foundation
-
 class Car: Vehicle
 {
     var identificationNumber: String
-    
     var description: String
-    
-    var isSelfDrive: Bool
-    
+    var  isSelfDrive: Bool
     var manufacturerName: String
-    
-    var isInsured: Bool
-    
-    var vehicleNoOfSeats: Int
-    
+    var  isInsured: Bool
+    var  vehicleNoOfSeats: Int
     var insuranceProviderName: String
-    
     var seat: Int
-    
     var fuelType: String
-    
     var ratePerDay: Double
-    
     var ratePerKm: Double
-    
-    required init(identificationNumber: String, description: String, isSelfDrive: Bool, manufacturerName: String, isInsured: Bool, vehicleNoOfSeats: Int, insuranceProviderName: String, seat: Int, fuelType: String, ratePerDay: Double, ratePerKm: Double) {
-        <#code#>
-    }
     
     var carType: String
     var carColor: String
-    init(identificationNumber:String,description:String,isSelfDrive:Bool,
-         manufacturerName: String,isInsured: Bool,vehicleNoOfSeats:Int,insuranceProviderName: String, seat: Int,fuelType: String,ratePerDay: Double,ratePerKm: Double, carType: String, carColor: String) {
-        self.carColor = carColor
-        self.carType = carType
-        
-    }
     
-     func Display() {
+    init(identificationNumber:String,description:String,isSelfDrive:Bool,
+         manufacturerName: String,isInsured: Bool,vehicleNoOfSeats:Int,insuranceProviderName: String, seat: Int,fuelType: String,ratePerDay: Double,ratePerKm: Double, carColor: String, carType: String)
+    {
+       self.identificationNumber = identificationNumber
+       self.description = description
+       self.isSelfDrive = isSelfDrive
+       self.manufacturerName = manufacturerName
+       self.isInsured = isInsured
+       self.vehicleNoOfSeats = vehicleNoOfSeats
+       self.insuranceProviderName = insuranceProviderName
+       self.seat = seat
+       self.fuelType = fuelType
+       self.ratePerDay = ratePerDay
+       self.ratePerKm = ratePerKm
+       self.carColor = carColor
+       self.carType = carType
+    
+      }
+    
+    
+     func display() {
         print("carColor:        \(carColor)")
         print("carType:         \(carType)")
     }
